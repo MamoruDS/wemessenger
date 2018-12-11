54ccdd740d93afb5f6f87bf55f259e3f87992043
MamoruDS <jlchen2015@gmail.com>
2018-12-11 14:51:38 +0800

    added wechat video received methods from alternative.js
    improved image message handle
    
    improved code clean

--- src/messenger.js
+++ src/messenger.js
@@ -1,9 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api')
-import * as axios from 'axios'
 import * as profile from './profile'
 import * as format from './format'
 import * as main from './main'
 import * as testProfile from './test_profile'
+import * as alternative from './alternative'
+import * as fs from 'fs'
 import {
     FileBox
 } from 'file-box'
@@ -13,6 +14,7 @@ const gm = require('gm').subClass({
 
 const token = testProfile.token
 const tgUser = testProfile.tgUser
+const wcUserAlias = testProfile.wcUser
 const tbot = new TelegramBot(token, {
     polling: true
 })
@@ -45,31 +47,22 @@ export const tgMessenger = (contact_id, room_id) => {
             // let tgBot = new TelegramBot(bot.token, {
             // polling: false
             // })
-            //TODO:
         }
     }
 }
 
 export const wechatMsgHandle = async (msg) => {
-    let contact = msg.from()
     let text = msg.text()
-    let room = msg.room()
-
     if (await checkConflict(msg)) return
 
-    console.log('\n')
-    console.log(JSON.stringify(contact).white)
-    console.log(JSON.stringify(room).blue)
-
     if (msg.type() === main.messageType.Attachment) {
-        console.log('MessageType: ' + 'Attachment'.red)
+        // attachment & subscription & url-share
+        // console.log('MessageType: ' + 'Attachment'.red)
         console.log(text)
         let text_test = format.decodeHTML(text)
         let url = text_test
             .replace(/(.*?\<url\>)/, '')
             .replace(/(\<\/url\>.*)/, '')
-        // .replace(/(\.*&lt;\/url&gt;)/, '')
-        // .replace(/(&lt;url&gt;.*?)/, '')
         if (url) {
             tbot.sendMessage(tgUser, format.parseWechatURL(text), {
                 parse_mode: 'HTML'
@@ -83,36 +76,56 @@ export const wechatMsgHandle = async (msg) => {
             })
         }
     } else if (msg.type() === main.messageType.Audio) {
-        console.log('MessageType: ' + 'Audio'.red)
-        //do not show wave diagram on mobile
+        // voice note message
+        // TODO: show sound wave on telegram macos/android/ios
         let file = await msg.toFileBox()
         let stream = await file.toStream()
         tbot.sendVoice(tgUser, stream)
     } else if (msg.type() === main.messageType.Contact) {
-        //NOT AVAILABLE FOR NOW
+        // unknown type of message
         console.log('MessageType: ' + 'Contact'.red)
     } else if (msg.type() === main.messageType.Emoticon) {
-        //NOT AVAILABLE FOR NOW
+        // unknown type of message
         console.log('MessageType: ' + 'Emoticon'.red)
     } else if (msg.type() === main.messageType.Image) {
-        // smail size gif not working
-        // console.log('MessageType: ' + 'Image'.red)
+        // normal image & custom sticker & gif & telegram sticker with file_id
         let file = await msg.toFileBox()
-        let stream = await file.toStream()
+        let buf = await file.toBuffer()
         if (/\.gif$/.test(file.name)) {
             if (/(tg_sticker_unique_file_id_.*?)/.test(file.name)) {
-                console.log('MessageType: ' + 'Image-sticker'.red)
                 let sticker_file_id = file.name
                 sticker_file_id = sticker_file_id.replace(/(tg_sticker_unique_file_id_.*?)/g, '')
                 sticker_file_id = sticker_file_id.replace(/\.gif$/g, '')
                 tbot.sendSticker(tgUser, sticker_file_id)
             } else {
-                console.log('MessageType: ' + 'Image-gif-normal'.red)
-                // console.log(file)
-                // console.log('length: '.green + file.toBuffer().toString().length)
-                // await file.toFile('tmp/test.gif')
-                tbot.sendDocument(tgUser, stream, {}, {
-                    filename: file.name
+                let image = gm(buf, file.name)
+                let tmp_name = Date.now().toString()
+                image.identify(async (err, info) => {
+                    if (!info.Scene) {
+                        console.log('MessageType: ' + 'Image-gif-sticker'.red)
+                        image.setFormat('WebP').write(`tmp/${tmp_name}.webp`, async (err) => {
+                            if (err) {
+                                console.log(err)
+                            }
+                            tbot.sendDocument(tgUser, `tmp/${tmp_name}.webp`)
+                            fs.unlinkSync(`tmp/${tmp_name}.webp`)
+                        })
+                    } else {
+                        console.log('MessageType: ' + 'Image-gif-gif'.red)
+                        // TODO: gif with small size will shown as an attachment on telegram
+                        // image.coalesce()
+                        //     .resize(200, 200)
+                        //     .toBuffer('gif', async (err, buffer) => {
+                        //         if (err) {
+                        //             console.log('[ERR]'.red + err)
+                        //         } else {
+                        //             tbot.sendDocument(tgUser, buffer)
+                        //         }
+                        //     })
+                        tbot.sendDocument(tgUser, buf, {}, {
+                            filename: file.name
+                        })
+                    }
                 })
             }
         } else {
@@ -121,30 +134,23 @@ export const wechatMsgHandle = async (msg) => {
             tbot.sendPhoto(tgUser, stream)
         }
     } else if (msg.type() === main.messageType.Text) {
+        // text message
         console.log('MessageType: ' + 'Text'.red)
         tbot.sendMessage(tgUser, text)
     } else if (msg.type() === main.messageType.Video) {
+        // video message
         console.log('MessageType: ' + 'Video'.red)
-        // Unhandled rejection Error: ETELEGRAM: 400 Bad Request: file must be non-empty
         let file = await msg.toFileBox()
-        let stream = await file.toBuffer()
-        // tbot.sendVideo(tgUser, file.remoteUrl)
-        console.log(stream.toString().length)
-        axios.request(file.remoteUrl, {
-                headers: file.headers,
-                proxy: false
-            })
-            .then((res) => {
-                console.log(res.data)
-                console.log(typeof res.data)
-                tbot.sendVideo(tgUser, Buffer.from(res.data))
-            })
+        let buf = await alternative.wechatVideoBuffer(file)
+        tbot.sendVideo(tgUser, buf)
     } else if (msg.type() === main.messageType.Url) {
+        // unknown type of message
         console.log('MessageType: ' + 'Url'.red)
         tbot.sendMessage(tgUser, format.parseWechatURL(text), {
             parse_mode: 'HTML'
         })
     } else {
+        // unknown type of message
         console.log('MessageType: ' + 'Other'.red)
         // tbot.sendMessage(tgUser, text)
         console.log(msg)
@@ -152,40 +158,52 @@ export const wechatMsgHandle = async (msg) => {
 }
 
 tbot.on('message', async (msg) => {
-    // console.log(msg)
-    let text = msg.text
-    if (text) {
-        let mamoru = await main.bot.Contact.find({
-            alias: 'mamoru-test'
-        })
-        if (mamoru) {
-            mamoru.say(text)
-            setMsgConflict(mamoru.id, 'text', text)
+    console.log(msg)
+    let wcUser = await main.bot.Contact.find({
+        alias: wcUserAlias
+    })
+    if (wcUser) {
+        let text = msg.text
+        if (text) {
+            wcUser.say(text)
+            setMsgConflict(wcUser.id, 'text', text)
+        }
+        let sticker = msg.sticker
+        if (sticker) {
+            let file_link = await tbot.getFileLink(sticker.file_id)
+            let file = await FileBox.fromUrl(file_link)
+            let buf = await file.toBuffer()
+            gm(buf).toBuffer('gif', async (err, buffer) => {
+                if (err) console.error(err)
+                file = await FileBox.fromBuffer(buffer, `tg_sticker_unique_file_id_${sticker.file_id}.gif`)
+                wcUser.say(file)
+                setMsgConflict(wcUser.id, 'sticker', `tg_sticker_unique_file_id_${sticker.file_id}.gif`)
+            })
+        }
+        let animation = msg.animation
+        if (animation) {
+            let file_link = await tbot.getFileLink(animation.file_id)
+            let file = await FileBox.fromUrl(file_link, animation.file_name)
+            wcUser.say(file)
+            file.toFile('tmp/' + animation.file_name)
+            setMsgConflict(wcUser.id, 'gif', animation.file_name)
+        }
+        let video_note = msg.video_note
+        if (video_note) {
+            // TODO: file & video sending issue
+            // let file_link = await tbot.getFileLink(video_note.file_id)
+            // console.log(file_link)
+            // let file = await FileBox.fromUrl(file_link)
+            // // let buf = await file.toBuffer()
+            // wcUser.say(file)
+            // // file.toFile('tmp/' + video_note.file_name)
+            // // setMsgConflict(wcUser.id, 'gif', video_note.file_name)
+        }
+        let voice = msg.voice
+        if (voice) {
+            // TODO: web wechat doesn't support voice message sending, waiting for windows-client puppet(?)
         }
     }
-    let sticker = msg.sticker
-    if (sticker) {
-        // let file_link = await tbot.getFileLink(sticker.file_id)
-        // console.log(file_link)
-        // let file = await FileBox.fromUrl(file_link)
-        let mamoru = await main.bot.Contact.find({
-            alias: 'mamoru-test'
-        })
-        let file_link = await tbot.getFileLink(sticker.file_id)
-        console.log(file_link)
-        let file = await FileBox.fromUrl(file_link)
-        let buf = await file.toBuffer()
-
-        gm(buf).toBuffer('gif', async (err, buffer) => {
-            if (err) console.error(err)
-            file = await FileBox.fromBuffer(buffer, `tg_sticker_unique_file_id_${sticker.file_id}.gif`)
-            if (mamoru) {
-                mamoru.say(file)
-                setMsgConflict(mamoru.id, 'sticker', `tg_sticker_unique_file_id_${sticker.file_id}.gif`)
-            }
-        })
-    }
-
 })
 
 export const telegramMsgHandle = () => {
@@ -197,6 +215,10 @@ const setMsgConflict = (id, type, content) => {
         type = main.messageType.Text
     } else if (type === 'sticker') {
         type = main.messageType.Image
+    } else if (type === 'gif') {
+        type = main.messageType.Video
+    } else if (type === 'audio') {
+        type = main.messageType.Attachment
     } else {
         type = main.messageType.Text
         content = 'unknown telegram message'
@@ -221,6 +243,10 @@ const checkConflict = async (msg) => {
                         return true
                     }
                 } else if (type === main.messageType.Image) {
+                    return await filenameCheck(msg, msgCheck.content)
+                } else if (type === main.messageType.Video) {
+                    return await filenameCheck(msg, msgCheck.content)
+                } else if (type === main.messageType.Attachment) {
                     return await filenameCheck(msg, msgCheck.content)
                 }
             } else {
