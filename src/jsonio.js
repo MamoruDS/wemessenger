3fff724e427ae3f6c91f6c3c04f9a7bb680cf304
MamoruDS <jlchen2015@gmail.com>
2018-12-20 15:39:30 +0800

    added expect format to readJSON and writeJSON

--- src/jsonio.js
+++ src/jsonio.js
@@ -1,17 +1,17 @@
 import fs from 'fs'
 import beautify from 'js-beautify'
 
-export const readJSON = (json_path) => {
-    if (fs.existsSync(json_path)){
+export const readJSON = (json_path, expect_format = {}) => {
+    if (fs.existsSync(json_path)) {
         return JSON.parse(fs.readFileSync(json_path, 'utf8'))
     }
-    writeJSON(json_path, {})
-    return false 
+    writeJSON(json_path, expect_format)
+    return false
 }
 
-export const writeJSON = (json_path, data, default_data = {}) => {
+export const writeJSON = (json_path, data, empty_format = {}) => {
     if (!data) {
-        data = default_data
+        data = empty_format
     }
     data = JSON.stringify(data)
     data = beautify(data, {
