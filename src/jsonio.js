4dc2c42c9162405129c4d7bb1af957119fb9816e
MamoruDS <jlchen2015@gmail.com>
2019-01-04 12:06:33 +0800

    fixed readJSON do not return expect_format when file not exist

--- src/jsonio.js
+++ src/jsonio.js
@@ -4,9 +4,10 @@ import beautify from 'js-beautify'
 export const readJSON = (json_path, expect_format = {}) => {
     if (fs.existsSync(json_path)) {
         return JSON.parse(fs.readFileSync(json_path, 'utf8'))
+    } else {
+        writeJSON(json_path, expect_format)
+        return readJSON(json_path, expect_format)
     }
-    writeJSON(json_path, expect_format)
-    return false
 }
 
 export const writeJSON = (json_path, data, empty_format = {}) => {
