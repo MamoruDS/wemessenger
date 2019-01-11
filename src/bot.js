41988c34a8bc26f00e10c999c41ee602d4760622
MamoruDS <jlchen2015@gmail.com>
2019-01-11 14:55:21 +0800

    modified W2TMsg bot send message handle
    added message response error handle method reqEnd
    
    added message response handle method reqEnd
    
    added method reqNextBot to call message resend by 'next' bot
    
    improved arguments now setting with dependency yargs
    
    added exiting notice

--- src/bot.js
+++ src/bot.js
@@ -1,41 +1,171 @@
+process.env.NTBA_FIX_319 = true
+process.env.NTBA_FIX_350 = true
+
 const TelegramBot = require('node-telegram-bot-api')
 import * as fs from 'fs'
 import * as msgHistory from './msgHistory'
+import {
+    argv
+} from 'yargs'
 
-const token = process.argv[2]
-const botId = process.argv[3]
+const token = argv['token']
+const botId = argv['id']
+const polling = argv['polling']
 
 if (!token) {
     // console.error('')
     process.exit(1)
 }
 
+process.on('message', async (msg) => {
+    W2TMsg(msg)
+})
+
+process.on('exit', () => {
+    console.log(`bot: ${botId} is exiting...`)
+})
+
 const bot = new TelegramBot(token, {
-    polling: true
+    polling: polling
 })
 
 bot.on('message', async (msg) => {
     T2WMsg(msg)
 })
 
-process.on('message', async (msg) => {
-    W2TMsg(msg)
-})
+/**
+ * @typedef pendingMsg
+ * @type {Object}
+ * @property {Array.<String>} botId array of pending bot id
+ * @property {String} chatId message recipient's telegram id
+ * @property {String} contactId current chat's contact id
+ * @property {(String|Buffer)} msgData
+ * @property {String} msgType
+ * @property {Boolean} isBuffer
+ * @property {Object} options telegram api options
+ * @property {String} options.reply_markup
+ * @property {String} options.filename
+ * @property {String} prefixStr
+ */
 
-const msgRecord = (res, contactId) => {
-    // TODO: record bot msg for reply
-    msgHistory.recordMsg(botId, res.message_id, res.date, contactId)
+/**
+ * replyPrefix
+ * @param {String} prefix 
+ * @returns {String} return prefix with 'sent this'
+ */
+const replyPrefix = prefix => {
+    if (prefix.match(/.*<code\>to\<\/code\>.*/)) {
+        return prefix.replace(/(.*)\s\<code\>to\<\/code\>\s(.*)$/g, (prefixStr, send, recv) => {
+            return `${send} <i>sent this</i> <code>to</code> ${recv}`
+        })
+    } else {
+        return `${prefix} <i>sent this</i>`
+    }
 }
 
+/**
+ * reqErrorCatch
+ * @param {Object} res 
+ * @param {Object} res.response
+ * @param {pendingMsg} msg
+ */
+const reqErrorCatch = (res, msg) => {
+    const resInfo = res.response.body
+    if (!resInfo.ok && msg) {
+        if (resInfo.description === 'Bad Request: chat not found') {
+            reqNextBot(msg)
+        } else if (resInfo.description === 'Bad Request: chat not found' || resInfo.error_code === 403) {
+            reqNextBot(msg)
+        }
+    }
+}
+
+/**
+ * reqEnd
+ * @param {Object} res 
+ * @param {String} contactId 
+ * @param {Boolean} needReply
+ * @param {Object} replyPrefix 
+ */
+const reqEnd = (res, contactId, needReply = false, replyPrefix = {
+    chatId: undefined,
+    prefix: undefined,
+    options: undefined,
+    fileOptions: undefined
+}) => {
+    if (res) {
+        if (contactId) {
+            msgRecord(res, contactId)
+        }
+        if (replyPrefix.prefix && needReply && res.message_id) {
+            let prefix = replyPrefix.prefix
+            if (prefix.match(/.*<code\>to\<\/code\>.*/)) {
+                prefix =  prefix.replace(/(.*)\s\<code\>to\<\/code\>\s(.*)$/g, (prefixStr, send, recv) => {
+                    return `${send} <i>sent this</i> <code>to</code> ${recv}`
+                })
+            } else {
+                prefix = `${prefix} <i>sent this</i>`
+            }
+            replyPrefix.options.reply_to_message_id = res.message_id
+            bot.sendMessage(replyPrefix.chatId, prefix, replyPrefix.options, replyPrefix.fileOptions).then((res) => {
+                if (contactId) {
+                    msgRecord(res, contactId)
+                }
+            })
+        }
+    }
+}
+
+/**
+ * reqNextBot
+ * @param {pendingMsg} msg 
+ */
+const reqNextBot = (msg) => {
+    if (msg.botId.length > 1) {
+        console.log('[INFO] request new bot.')
+        // msg.botId.shift()
+        msg.botId = msg.botId.slice(1)
+        // msg.prefixStr.shift()
+        msg.prefixStr = msg.prefixStr.slice(1)
+        process.send({
+            code: 'ETELEGRAM',
+            fromBotId: botId,
+            retry: true,
+            msg: msg
+        })
+    } else {
+        console.log('[INFO] no more bot.')
+    }
+}
+
+/**
+ * msgRecord
+ * @param {Object} res telegram API request's response object
+ * @param {(Number|String)} res.message_id telegram message id
+ * @param {(Number|String)} res.date telegram message timestamp
+ * @param {String} contactId current chat's contact id
+ */
+const msgRecord = (res, contactId) => {
+    // TODO: record bot msg for reply
+    // msgHistory.recordMsg(botId, res.message_id, res.date, contactId)
+    return
+}
+
+/**
+ * W2TMsg
+ * @param {pendingMsg} msg 
+ */
 const W2TMsg = (msg) => {
     const contactId = msg.contactId
     let data = msg.msgData
     let options = {}
     let fileOptions = {}
-    let prefix = msg.prefixStr || ''
+    let prefix = msg.prefixStr[0] || ''
+    if (prefix) {
+        options.caption = prefix
+    }
     options.parse_mode = 'HTML'
     options.reply_markup = msg.options.reply_markup
-    if (prefix) options.caption = prefix
     fileOptions.filename = msg.options.filename || undefined
     if (msg.isBuffer) {
         data = Buffer.from(msg.msgData)
@@ -44,59 +174,117 @@ const W2TMsg = (msg) => {
     switch (msg.msgType) {
         case 'message':
             if (prefix) data = `${msg.prefixStr}\n${data}`
-            bot.sendMessage(msg.chatId, data, options, fileOptions).then((res) => {
-                msgRecord(res, contactId)
-            })
+            bot.sendMessage(msg.chatId, data, options, fileOptions)
+                .catch((err) => {
+                    reqErrorCatch(err, msg)
+                })
+                .then((res) => {
+                    reqEnd(res, contactId, false, {
+                        chatId: msg.chatId,
+                        prefix: prefix,
+                        options: options,
+                        fileOptions: fileOptions
+                    })
+                })
             break
         case 'photo':
-            bot.sendPhoto(msg.chatId, data, options, fileOptions).then((res) => {
-                msgRecord(res, contactId)
-            })
+            bot.sendPhoto(msg.chatId, data, options, fileOptions)
+                .catch((err) => {
+                    reqErrorCatch(err, msg)
+                })
+                .then((res) => {
+                    reqEnd(res, contactId, false, {
+                        chatId: msg.chatId,
+                        prefix: prefix,
+                        options: options,
+                        fileOptions: fileOptions
+                    })
+                })
             break
         case 'audio':
-            bot.sendAudio(msg.chatId, data, options, fileOptions).then((res) => {
-                msgRecord(res, contactId)
-            })
+            bot.sendAudio(msg.chatId, data, options, fileOptions)
+                .catch((err) => {
+                    reqErrorCatch(err, msg)
+                })
+                .then((res) => {
+                    reqEnd(res, contactId, true, {
+                        chatId: msg.chatId,
+                        prefix: prefix,
+                        options: options,
+                        fileOptions: fileOptions
+                    })
+                })
             break
         case 'document':
             options.caption = undefined
             bot.sendDocument(msg.chatId, data, options, fileOptions)
+                .catch((err) => {
+                    reqErrorCatch(err, msg)
+                })
                 .then((res) => {
-                    msgRecord(res, contactId)
-                    if (prefix) {
-                        options.reply_to_message_id = res.message_id
-                        bot.sendMessage(msg.chatId, `${prefix} <i>sent this</i>`, options, fileOptions).then((res) => {
-                            msgRecord(res, contactId)
-                        })
-                    }
+                    reqEnd(res, contactId, true, {
+                        chatId: msg.chatId,
+                        prefix: prefix,
+                        options: options,
+                        fileOptions: fileOptions
+                    })
                 })
             break
         case 'sticker':
             bot.sendSticker(msg.chatId, data, options, fileOptions)
+                .catch((err) => {
+                    reqErrorCatch(err, msg)
+                })
                 .then((res) => {
-                    msgRecord(res, contactId)
-                    if (prefix) {
-                        options.reply_to_message_id = res.message_id
-                        bot.sendMessage(msg.chatId, `${prefix} <i>sent this</i>`, options, fileOptions).then((res) => {
-                            msgRecord(res, contactId)
-                        })
-                    }
+                    reqEnd(res, contactId, true, {
+                        chatId: msg.chatId,
+                        prefix: prefix,
+                        options: options,
+                        fileOptions: fileOptions
+                    })
                 })
             break
         case 'video':
-            bot.sendVideo(msg.chatId, data, options, fileOptions).then((res) => {
-                msgRecord(res, contactId)
-            })
+            bot.sendVideo(msg.chatId, data, options, fileOptions)
+                .catch((err) => {
+                    reqErrorCatch(err, msg)
+                })
+                .then((res) => {
+                    reqEnd(res, contactId, false, {
+                        chatId: msg.chatId,
+                        prefix: prefix,
+                        options: options,
+                        fileOptions: fileOptions
+                    })
+                })
             break
         case 'videoNote':
-            bot.sendVideoNote(msg.chatId, data, options, fileOptions).then((res) => {
-                msgRecord(res, contactId)
-            })
+            bot.sendVideoNote(msg.chatId, data, options, fileOptions)
+                .catch((err) => {
+                    reqErrorCatch(err, msg)
+                })
+                .then((res) => {
+                    reqEnd(res, contactId, false, {
+                        chatId: msg.chatId,
+                        prefix: prefix,
+                        options: options,
+                        fileOptions: fileOptions
+                    })
+                })
             break
         case 'voice':
-            bot.sendVoice(msg.chatId, data, options, fileOptions).then((res) => {
-                msgRecord(res, contactId)
-            })
+            bot.sendVoice(msg.chatId, data, options, fileOptions)
+                .catch((err) => {
+                    reqErrorCatch(err, msg)
+                })
+                .then((res) => {
+                    reqEnd(res, contactId, true, {
+                        chatId: msg.chatId,
+                        prefix: prefix,
+                        options: options,
+                        fileOptions: fileOptions
+                    })
+                })
             break
         case 'location':
             // bot.sendLocation(msg.chatId, data)
@@ -107,6 +295,10 @@ const W2TMsg = (msg) => {
     }
 }
 
+/**
+ * T2WMsg
+ * @param {Object} msg 
+ */
 const T2WMsg = (msg) => {
     process.send(msg)
 }
